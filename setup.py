7b73edcbdaabf664adfb054163b11d6769cdac2a
Chandan Kumar <115529518+Chandan124497@users.noreply.github.com>
2026-06-15 17:07:55 +0530

    commited

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+
+def get_requirements()-> list(str):
+    requirements_list = list[str]=[]
+
+    return requirements_list
+    
 setup(
     name="sensor",
     version="0.0.1",
     author="Chandan Kumar",
     author_email="chandankm825312@gmail.com",
     packages=find_packages(),
-    install_requires=["pymongo"]
+    install_requires=get_requirements()
 )
